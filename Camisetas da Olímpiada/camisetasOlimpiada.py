a51041ac902996e3a26b4c557ac9535dbb043025
Gustavo de Oliveira <go609127@gmail.com>
2022-04-17 20:33:00 -0300

    camisetas Olimpiada
    Adicionei as restrições ao código

--- Camisetas da Olímpiada/camisetasOlimpiada.py
+++ Camisetas da Olímpiada/camisetasOlimpiada.py
@@ -5,20 +5,23 @@ nMedia = int(input())
 sAux = str(sCamisetas)
 
 def fnContaCamisa():
-    nCamisas = list(sAux)
-    i = 0
-    nNum1 = 0
-    nNum2 = 0
-    while i < len(nCamisas):
-        if nCamisas[i] == '1':
-            nNum1 += 1
-        else:
-            nNum2 += 1
-        i += 1
-    if nPequena == nNum1 and nMedia == nNum2 and nAlunos == len(nCamisas):
-        print("S")
-    else:
-        print("N")
+    if nAlunos >= 0 and nAlunos <= 1000:
+        if nPequena >= 0 and nPequena <= 1000 and nMedia >= 0 and nMedia <= 1000:
+            if nAlunos == (nPequena + nMedia):
+                nCamisas = list(sAux)
+                i = 0
+                nNum1 = 0
+                nNum2 = 0
+                while i < len(nCamisas):
+                    if nCamisas[i] == '1':
+                        nNum1 += 1
+                    else:
+                        nNum2 += 1
+                    i += 1
+                if nPequena == nNum1 and nMedia == nNum2 and nAlunos == len(nCamisas):
+                    print("S")
+                else:
+                    print("N")
     
 
 fnContaCamisa()
