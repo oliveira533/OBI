326e1159e38602a3450a2b49b56cb43b3916f528
Gustavo de Oliveira <go609127@gmail.com>
2022-05-05 21:34:03 -0300

    Update zero.py
    agora sim tá funcionando
    
    fiz uma grande gambiarra

--- Zero para cancelar/zero.py
+++ Zero para cancelar/zero.py
@@ -1,17 +1,27 @@
 nQuantidade = int(input()) # peguei a quantidade de valores que vão ser inseridos
-nValores = [] # criei a lista dos valores
+x = 0 # criei uma variavel de contador
+sAux= '' # crio uma variavel auxiliar
+i = 0 # criei mais  uma variavel de contador 
+
+# agora eu faço uma grande gambiarra para poder criar uma lista do tamanho que é preciso
+while i < nQuantidade:
+    sAux += 'a'
+    i+=1
+nValores = list(sAux)
 
 # bloco de código para para pegar os valores da lista
-for x in range(nQuantidade):
-    nValores[nQuantidade] = int(input())
+while x < nQuantidade:
+    nValores[x] = int(input())
+    x+=1
+# print(nValores[0])
 
 nCont = 0 # crio um contador
 nLimite = len(nValores) # defino uma variável para ser usada como limete do meu contador
 i = 0 # variável de contadore
 nResultado = 0 # variável utilizada para guardar vaolor final
 
-while nCont != nLimite:
-    if nValores[nCont] == '0' and nValores[(nCont-1)] != '1': # testo se o valor é 0 ou 10
+while nCont < nLimite:
+    if nValores[nCont] == 0 and nValores[(nCont-1)] != 0: # testo se o valor é 0 ou 10
         del(nValores[(nCont-1)]) # deleto o valor anterior ao 0
         del(nValores[(nCont-1)]) # deleto o 0
         nCont-=2 # volto o contador 2 números antes para poder recomeçar
@@ -19,11 +29,11 @@ while nCont != nLimite:
         
     nCont+=1
 
-del(nValores[0]) # deleto o primeiro valor, pois ele é ignorado na conta
+# del(nValores[0]) # deleto o primeiro valor, pois ele é ignorado na conta
 
 # bloco de código para fazer a soma de todos os valores da lista
 while i != len(nValores):
     nResultado = nResultado + int(nValores[i])
     i+=1
 
-print(nResultado) # exibição de resultado
+print(int(nResultado)) # exibição de resultado
